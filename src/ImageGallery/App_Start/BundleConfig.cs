295c2272fed2d3ddccf0135337a75af980875ab9
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-04-18 12:42:14 +0300

    Added KendoUI

--- src/ImageGallery/App_Start/BundleConfig.cs
+++ src/ImageGallery/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ImageGallery
 {
@@ -8,24 +7,57 @@ namespace ImageGallery
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.IgnoreList.Clear();
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            RegisterScriptBundles(bundles);
+            RegisterContentBundles(bundles);
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = false;
+        }
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+        private static void RegisterContentBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(
+                new StyleBundle("~/Content/kendo").Include(
+                    "~/Content/kendo/kendo.common.min.css",
+                    "~/Content/kendo/kendo.common-bootstrap.min.css"));
+
+            bundles.Add(
+                new StyleBundle(
+                    "~/content/toastr",
+                    "http://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css").Include(
+                        "~/Content/toastr.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include("~/Content/site.css"));
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
+            bundles.Add(
+                new ScriptBundle("~/bundles/kendo").Include(
+                    "~/Scripts/kendo/kendo.web.min.js",
+                    "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/kendo/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include("~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include("~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(
+                new ScriptBundle(
+                    "~/bundles/toastr",
+                    "http://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js").Include(
+                        "~/Scripts/toastr.min.js"));
+
+            bundles.Add(
+                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
         }
     }
 }
